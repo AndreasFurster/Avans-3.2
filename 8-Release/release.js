371e0b4a967819d177504d0878e2cc38c2b6095d
Andreas Furster <andreas.furster@hotmail.com>
2020-01-16 00:15:12 +0100

    Updated release

--- 8-Release/release.js
+++ 8-Release/release.js
@@ -1,11 +1,18 @@
 const axios = require('axios');
-const url = 'https://hook.integromat.com/2zabcvfrefsnlkr0dypo5vvojij7mr6o'
+const fs = require('fs');
 
+console.log('Getting the magic number for release info...');
+
+const magic = fs.readFileSync('../7-Presentation/magic-number.txt');
+console.log(`The magic number is: ${magic}`);
+
+const url = `https://hook.integromat.com/2zabcvfrefsnlkr0dypo5vvojij7mr6o?magic=${magic}`
+console.log('Triggering release webhook...');
 
 axios.get(url)
   .then(function (response) {
-    console.log(response);
+    console.log('Successfully triggerd release webhook!');
   })
   .catch(function (error) {
-    console.log(error);
+    throw new Error(error.message);
   })
